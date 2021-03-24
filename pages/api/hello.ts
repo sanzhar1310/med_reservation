292027680ea6fd6378ddb77e8ecf2f0d1bfbce8c
Sanzhar <sanzhar42@gmail.com>
2021-03-24 13:23:27 +0600

    Initial commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+export default (_req: NextApiRequest, res: NextApiResponse): void => {
   res.status(200).json({ name: 'John Doe' })
 }
